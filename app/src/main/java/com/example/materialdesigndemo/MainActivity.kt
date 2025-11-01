98329a8c2cf6c5a2bb4588fdcbaa4a82cf1753a7
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2025-11-01 11:37:25 -0400

    Feat: Material design examples

--- app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.materialdesigndemo.ui.theme.MaterialDesignDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialDesignDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        ButtonExample(
+                            text = "Boton",
+                            modifier = Modifier,
+                            action = { println("Click") }
+                        )
+
+                        CardExample(
+                            title = "Title",
+                            content = "Content",
+                            modifier = Modifier.width(100.dp)
+                                .height(100.dp)
+                        )
+
+                        NavigationBarExample(
+                            modifier = Modifier
+                        )
+                    }
                 }
             }
         }
@@ -31,17 +65,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialDesignDemoTheme {
-        Greeting("Android")
+fun ButtonExample(
+    text: String,
+    modifier: Modifier = Modifier,
+    action: () -> Unit
+) {
+    FilledTonalButton (
+        modifier = modifier,
+        onClick = action
+    ) {
+        Text(text = text)
     }
 }
+
+@Composable
+fun CardExample(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: String
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = title)
+            Text(text = content)
+        }
+    }
+}
+
+@Composable
+fun NavigationBarExample(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            selected = true,
+            icon = {
+                Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+            },
+            label = {
+                Text(text = "Home")
+            },
+            onClick = {
+                println("Home")
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            icon = {
+                Icon(imageVector = Icons.Default.Check, contentDescription = "Task")
+            },
+            label = {
+                Text(text = "Tasks")
+            },
+            onClick = {
+                println("Tasks")
+            }
+        )
+    }
+}
+
+
